265cc0cdc756e4e356196383f1811c8f18693055
Eduardo Leite Ranzzani <eduranzzani@gmail.com>
2021-04-15 09:15:45 -0400

    Mudança para componentes material

--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, IconButton, makeStyles, Typography } from '@material-ui/core';
+import { Link } from '@reach/router';
+import { Button, Card, CardActionArea, CardActions, CardContent, CardMedia, IconButton, makeStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@material-ui/core';
 
 import SearchTwoToneIcon from '@material-ui/icons/SearchTwoTone';
 import EditTwoToneIcon from '@material-ui/icons/EditTwoTone';
@@ -35,7 +35,7 @@ const modalData = {
     pokemonData: {}
 };
 
-const POKEMONS_PER_PAGE = 10;
+const POKEMON_PER_PAGE = 10;
 
 const PokemonList = () => {
     const [modalPokemon, setModalPokemon] = useState(modalData);
@@ -44,7 +44,7 @@ const PokemonList = () => {
     const [page, setPage] = useState(0);
     const classes = useStyles();
 
-    const apiUrl = `https://pokeapi.co/api/v2/pokemon?offset=${page}&limit=${POKEMONS_PER_PAGE}`;
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon?offset=${page}&limit=${POKEMON_PER_PAGE}`;
 
     useEffect(() => {
         axios.get(apiUrl).then(response => {
@@ -52,67 +52,74 @@ const PokemonList = () => {
         }, console.error);
     }, [apiUrl]);
 
-    function nextPage() {
+    const nextPage = () => {
         if (page === 0) {
-            setPage(POKEMONS_PER_PAGE);
+            setPage(POKEMON_PER_PAGE);
         } else if (pokemons.next) {
-            setPage(page + POKEMONS_PER_PAGE);
+            setPage(page + POKEMON_PER_PAGE);
         }
-    }
+    };
 
-    function previousPage() {
+    const previousPage = () => {
         if (pokemons.previous) {
-            setPage(page - POKEMONS_PER_PAGE);
+            setPage(page - POKEMON_PER_PAGE);
         }
-    }
+    };
 
     return (
         <div>
             <div className="box" >
-                <h1 className="title" >Exibir Pokémon</h1>
+                <Link to="/">
+                    <h1 className="title" >Exibir Pokémon</h1>
+                </Link>
 
                 <hr />
 
-                <table className="table is-striped is-fullwidth is-hoverable is-narrow">
-                    <thead>
-                        <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Nome</th>
-                            <th scope="col">URL</th>
-                            <th scope="col" style={{ width: '110px' }}>Opções</th>
-                        </tr>
-                    </thead>
+                <TableContainer>
+                    <Table aria-label="enhanced table" >
+                        <TableHead>
+                            <TableRow>
+                                <TableCell component="th">#</TableCell >
+                                <TableCell align="right">Nome</TableCell >
+                                <TableCell align="right">Url</TableCell >
+                                <TableCell align="right" style={{ width: '120px' }}>Opções</TableCell >
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {pokemons.results
+                                .map((pokemon, index) => {
+                                    const id = index + 1;
 
-                    <tbody>
-                        {pokemons && pokemons.results.map((pokemon, index) => {
-                            return (
-                                <tr key={(index + 1)}>
-                                    <th scope="row">{(index + 1) + page}</th>
-                                    <td className="capitalize" >{pokemon.name}</td>
-                                    <td>{pokemon.url}</td>
-                                    <td>
-                                        <IconButton color="primary" size="small" onClick={e => {
-                                            setState({ showModal: !state.showModal, selectedPokemon: pokemon });
-                                            axios.get(pokemon.url).then(response => setModalPokemon(response.data));
-                                        }} >
-                                            <SearchTwoToneIcon />
-                                        </IconButton>
-                                        <IconButton color="primary" disabled size="small">
-                                            <EditTwoToneIcon />
-                                        </IconButton>
-                                        <IconButton color="secondary" disabled size="small">
-                                            <DeleteTwoToneIcon />
-                                        </IconButton>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                    </tbody>
-                </table>
+                                    return (
+                                        <TableRow key={id} hover tabIndex={-1}>
+                                            <TableCell component="th" scope="row">{id + page}</TableCell>
+                                            <TableCell className="capitalize" >{pokemon.name}</TableCell >
+                                            <TableCell >{pokemon.url}</TableCell >
+                                            <TableCell >
+                                                <IconButton color="primary" size="small" onClick={e => {
+                                                    setState({ showModal: !state.showModal, selectedPokemon: pokemon });
+                                                    axios.get(pokemon.url).then(response => setModalPokemon(response.data));
+                                                }} >
+                                                    <SearchTwoToneIcon />
+                                                </IconButton>
+                                                <IconButton color="primary" disabled size="small">
+                                                    <EditTwoToneIcon />
+                                                </IconButton>
+                                                <IconButton color="secondary" disabled size="small">
+                                                    <DeleteTwoToneIcon />
+                                                </IconButton>
+                                            </TableCell >
+                                        </TableRow>
+                                    );
+                                })
+                            }
+                        </TableBody>
+                    </Table>
+                </TableContainer>
 
                 <nav className="pagination is-rounded is-small" role="navigation" aria-label="pagination">
-                    <Link to="/" onClick={e => previousPage()} className="pagination-previous">Página Anterior</Link>
-                    <Link to="/" onClick={e => nextPage()} className="pagination-next">Próxima Página</Link>
+                    <Link to="/" onClick={() => previousPage()} className="pagination-previous">Página Anterior</Link>
+                    <Link to="/" onClick={() => nextPage()} className="pagination-next">Próxima Página</Link>
                 </nav>
             </div>
             {
@@ -124,7 +131,7 @@ const PokemonList = () => {
                                     className={classes.media}
                                     image={
                                         modalPokemon.sprites
-                                            ? modalPokemon.sprites.other.dream_world.front_default
+                                            ? modalPokemon.sprites.front_default
                                             : "/teste"
                                     }
                                     title={modalPokemon.name}
@@ -148,7 +155,7 @@ const PokemonList = () => {
                                 </CardContent>
                             </CardActionArea>
                             <CardActions>
-                                <Button onClick={e => setState({ showModal: !state.showModal, selectedPokemon: {} })} size="small" color="primary">
+                                <Button onClick={() => setState({ showModal: !state.showModal, selectedPokemon: {} })} size="small" color="primary">
                                     Fechar
                                 </Button>
                             </CardActions>
